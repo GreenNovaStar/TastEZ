21475e17bed5419102f565eeea83263493504473
James Lord <caburb@gmail.com>
2020-10-27 11:04:53 -0700

    * ...

--- lib/recipe.book.dart
+++ lib/recipe.book.dart
@@ -2,10 +2,11 @@ import 'package:flutter/material.dart';
 import 'home.dart';
 
 Widget recipeBook() {
+
     return MaterialApp(
         debugShowCheckedModeBanner: false,
         home: Center(
-        child: Text('RecipeBook'),
+        child: Text("RecipeBook"),
     )
     );
 }

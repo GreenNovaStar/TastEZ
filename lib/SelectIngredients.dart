ad1f6142c79754cabf4a1338cb43839443d20083
GreenNovaStar <brianyip5@gmail.com>
2020-10-28 20:19:04 -0700

    Delete SelectIngredients.dart

--- lib/SelectIngredients.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'pantry.list.dart';
-
-Widget SelectIngredients1(){
-  return Scaffold(
-    appBar: AppBar(title: Text("SelectIngredients"),),
-    body: Center(child: Text('<null>')),
-  );
-}

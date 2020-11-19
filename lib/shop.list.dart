4518fb47838305fc84dd550a4ef8fa0b49f79c18
GreenNovaStar <brianyip5@gmail.com>
2020-11-19 11:50:18 -0800

    added strikethru to items crossed off on a tap, and unstrikethru on long press

--- lib/shop.list.dart
+++ lib/shop.list.dart
@@ -1,15 +1,22 @@
 import 'package:flutter/material.dart';
 import 'home.dart';
 
+class ShoppingListElement{
+  String ingredient;
+  bool   crossedOff;
+  ShoppingListElement({this.ingredient, this.crossedOff});
+}
+
 
 class _ShoppingListState extends State<ShoppingList> {
-  final List<String> _shoppingList = <String>[];//create a list to store strings
+  //final List<String> _shoppingList = <String>[];//create a list to store strings
+  List<ShoppingListElement> _shoppingList = [];
   final TextEditingController _textFieldController = TextEditingController();//create variable to get text input
 
   //adds a test item
   void addTest(){
     setState(() {
-      _shoppingList.add("Apples");
+      //_shoppingList.add("Apples");
     });
   }
 
@@ -18,7 +25,25 @@ class _ShoppingListState extends State<ShoppingList> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: ListView(children: _getItems()),
+        body: //ListView(children: _getItems()),
+          ListView.builder(
+              itemCount: (_shoppingList == null) ? 0 : _shoppingList.length,
+              itemBuilder: (context, i){
+                return ListTile(
+                  title: (_shoppingList[i].crossedOff && _shoppingList != null) ? Text(_shoppingList[i].ingredient, style: TextStyle(decoration: TextDecoration.lineThrough)) : Text(_shoppingList[i].ingredient, style: TextStyle(decoration: TextDecoration.none)),
+                  onTap: () {
+                    if(_shoppingList != null){
+                      setState(() {_shoppingList[i].crossedOff = true;});
+                    }
+                  },
+                  onLongPress: (){
+                    if(_shoppingList != null){
+                      setState(() {_shoppingList[i].crossedOff = false;});
+                    }
+                  },
+                );
+              }
+          ),
         floatingActionButton: FloatingActionButton.extended(
           onPressed: () => _displayDialog(context),
           icon: Icon(Icons.add),
@@ -33,9 +58,11 @@ class _ShoppingListState extends State<ShoppingList> {
   void _addShoppingItem(String title) {
     // Wrapping it inside a set state will notify
     // the app that the state has changed
-    setState(() {
-      _shoppingList.add(title);
-    });
+    if(title != ""){
+      setState(() {
+        _shoppingList.add(ShoppingListElement(ingredient: title, crossedOff: false));
+      });
+    }
     _textFieldController.clear();
   }
 
@@ -43,11 +70,12 @@ class _ShoppingListState extends State<ShoppingList> {
   bool _isSelected = false;
   // Generate list of item widgets
   Widget _buildShoppingItem(String title) {
-    print("in here");
+    print("title = $title");
     return Column(
       children:[
         ListTile(
           title: Text(title),
+          //onTap:
         ),
       ]
     );
@@ -94,13 +122,13 @@ class _ShoppingListState extends State<ShoppingList> {
         });
   }
 
-  List<Widget> _getItems() {
-    final List<Widget> _shopWidgets = <Widget>[];
-    for (String title in _shoppingList) {
-      _shopWidgets.add(_buildShoppingItem(title));
-    }
-    return  _shopWidgets;
-  }
+  // List<Widget> _getItems() {
+  //   final List<Widget> _shopWidgets = <Widget>[];
+  //   for (String title in _shoppingList) {
+  //     _shopWidgets.add(_buildShoppingItem(title));
+  //   }
+  //   return  _shopWidgets;
+  // }
 }
 
 //void main() => runApp(shopList());

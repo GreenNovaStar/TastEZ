4660209c601a4f14ca9b94fd6fc1f2b334ebdb85
GreenNovaStar <brianyip5@gmail.com>
2021-04-23 15:16:22 -0700

    ** Commit Date - April 23 2021 @ 03:16pm
    ** Changes
    - added an icon to view licenses
    
    ** TODO
    - Fix the navigation of the app

--- lib/GUI/Pages/About.dart
+++ lib/GUI/Pages/About.dart
@@ -14,9 +14,9 @@ class AboutPage extends StatelessWidget {
           // applicationIcon: Image.asset("assets/TastEZ_logo.png",width: 100,),
           // applicationIcon: FlutterLogo(),
           applicationVersion: '1.0.0',
-          children: [
-            Text('This is an About Dialog in Flutter'),
-          ]
+          // children: [
+          //   Text('This is an About Dialog in Flutter'),
+          // ]
       );
     }
 
@@ -36,56 +36,15 @@ class AboutPage extends StatelessWidget {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () => _showDialog(),
-        // onPressed: () {
-        //   AlertDialog(
-        //     content: ListBody(
-        //       children: <Widget>[
-        //         Row(
-        //           crossAxisAlignment: CrossAxisAlignment.start,
-        //           children: <Widget>[
-        //             if (Image.asset("assets/TastEZ_logo-v2.png",width: 100,), != null) IconTheme(data: Theme.of(context).iconTheme, child: icon),
-        //             Expanded(
-        //               child: Padding(
-        //                 padding: const EdgeInsets.symmetric(horizontal: 24.0),
-        //                 child: ListBody(
-        //                   children: <Widget>[
-        //                     Text("TastEZ", style: Theme.of(context).textTheme.headline5),
-        //                     Text("1.0.0", style: Theme.of(context).textTheme.bodyText2),
-        //                     const SizedBox(height: 18.0),
-        //                     Text(applicationLegalese ?? '', style: Theme.of(context).textTheme.caption),
-        //                   ],
-        //                 ),
-        //               ),
-        //             ),
-        //           ],
-        //         ),
-        //         ...?children,
-        //       ],
-        //     ),
-        //     actions: <Widget>[
-        //       TextButton(
-        //         child: Text(MaterialLocalizations.of(context).viewLicensesButtonLabel),
-        //         onPressed: () {
-        //           showLicensePage(
-        //             context: context,
-        //             applicationName: applicationName,
-        //             applicationVersion: applicationVersion,
-        //             applicationIcon: applicationIcon,
-        //             applicationLegalese: applicationLegalese,
-        //           );
-        //         },
-        //       ),
-        //       TextButton(
-        //         child: Text(MaterialLocalizations.of(context).closeButtonLabel),
-        //         onPressed: () {
-        //           Navigator.pop(context);
-        //         },
-        //       ),
-        //     ],
-        //     scrollable: true,
-        //   );
-        // },
+        backgroundColor: themeColor,
+        // onPressed: () => _showDialog(),
+        child: Icon(Icons.article_outlined),
+        onPressed: () => showLicensePage(
+          context: context,
+          applicationVersion: '1.0.0',
+          applicationName: 'TastEZ',
+          applicationIcon: Image.asset("assets/TastEZ_logo-v2.png",width: 100,),
+        ),
       ),
     );
   }
